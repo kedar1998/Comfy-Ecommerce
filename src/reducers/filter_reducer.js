df71fcb4fee1ca919c1897ab389050e5a9c0f722
Kedar <kedarmakode1598@gmail.com>
2022-12-22 10:56:56 +0530

    add sort functionality and view functionality for products

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -1,3 +1,4 @@
+import { act } from 'react-dom/test-utils'
 import {
   LOAD_PRODUCTS,
   SET_LISTVIEW,
@@ -10,7 +11,56 @@ import {
 } from '../actions'
 
 const filter_reducer = (state, action) => {
-  return state
+
+  if(action.type === LOAD_PRODUCTS){
+    return {...state, all_products: [...action.payload], filtered_products: [...action.payload]}
+  }
+
+  if(action.type === SET_GRIDVIEW){
+    return {...state, grid_view: true}
+  }
+
+  if(action.type === SET_LISTVIEW){
+    return {...state, grid_view: false}
+  }
+
+  if(action.type === UPDATE_SORT){
+    return {...state, sort: action.payload}
+  }
+
+  if(action.type === SORT_PRODUCTS){
+    
+    const {sort, filtered_products} = state
+    let tempProduct = [...filtered_products]
+
+    if(sort === 'price-lowest'){
+      tempProduct = tempProduct.sort((a,b) =>{
+        return a.price - b.price
+      })
+    }
+
+    if(sort === 'price-highest'){
+      tempProduct = tempProduct.sort((a,b) =>{
+        return b.price - a.price
+      })
+    }
+
+    if(sort === 'name-a'){
+      tempProduct = tempProduct.sort((a,b) =>{
+        return a.name.localeCompare(b.name) 
+      })
+    }
+
+    if(sort === 'name-z'){
+      tempProduct = tempProduct.sort((a,b) =>{
+        return b.name.localeCompare(a.name) 
+      })
+    }
+
+    
+    return {...state, filtered_products: tempProduct}
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
